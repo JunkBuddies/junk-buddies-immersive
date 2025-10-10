f6382bff96bf3afbb9cf2b2890daa9d357cf65fc
JunkBuddies <JunkBuddies.info@gmail.com>
2025-10-09 22:16:21 -0700

    Update LayoutShell.jsx

--- src/components/LayoutShell.jsx
+++ src/components/LayoutShell.jsx
@@ -1,27 +1,30 @@
 // LayoutShell.jsx
 import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
 import TopNav from "./TopNav";
+import TopNavMobile from "./TopNavMobile";
 import SideNav from "./SideNav";
 import ChatWidget from "./chat/ChatWidget";
 
 export default function LayoutShell() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 1024);
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className="flex h-screen bg-black text-white overflow-hidden">
-      {/* Sidebar (desktop) */}
-      <SideNav />
-
-      {/* Main Content */}
+      {!isMobile && <SideNav />}
       <div className="flex-1 flex flex-col">
-        {/* Top Navigation Bar */}
-        <TopNav />
-
-        {/* Page Content */}
+        {isMobile ? <TopNavMobile /> : <TopNav />}
         <main className="flex-1 overflow-y-auto scroll-smooth">
           <Outlet />
         </main>
       </div>
-
-      {/* Floating Chat Widget */}
       <ChatWidget />
     </div>
   );
